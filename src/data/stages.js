73ed001eabb6b7b534ab34bf632f7599e56c1ff0
rehtropsmb <kingretrop@gmail.com>
2024-09-24 02:11:26 -0500

    add days 2 and 3

--- src/data/stages.js
+++ src/data/stages.js
@@ -1,8 +1,17 @@
 const stages = [
-    { name: "Visionary", pack: "SMB Gaiden", slot: "8-9" },
-    { name: "Influence", pack: "SMB Launch!", slot: "8-7" },
-    { name: "Tick-Flip", pack: "SMB Stardust", slot: "3-6" },
+    { name: "Visionary", pack: "Gaiden", slot: "8-9" },
+    { name: "Giant Swing", pack: "SMB2", slot: "Expert Extra 4"},
+    { name: "Imbalance", pack: "Stardust", slot: "6-2" },
+    { name: "Influence", pack: "Launch!", slot: "8-7" },
+    { name: "Tick-Flip", pack: "Stardust", slot: "3-6" },
     { name: "", pack: "", slot: "" },
+    { name: "8 Bracelets", pack: "SMB2", slot: "Expert 29" },
+    { name: "Whiskers", pack: "Invasion", slot: "8-6" },
+    { name: "Cartons", pack: "Launch!", slot: "2-8" },
+    { name: "Shooting Stars", pack: "Stardust", slot: "10-1" },
+    { name: "Distort", pack: "Launch!", slot: "5-1" },
+    { name: "Klein Bottle", pack: "Stardust", slot: "1-9" },
+    { name: "Heavy Sphere", pack: "SMB2", slot: "Advanced Extra 2" },
 ];
 
 export default stages;
